2f4e916fe58776ed5f017b85649c3a851c39d1c8
Vadim <Vadim_lachkov@mail.ru>
2021-08-20 18:18:22 +0700

    implemented controllers methods

--- server/index.js
+++ server/index.js
@@ -5,13 +5,17 @@ const sequelize = require("./db.js");
 const models = require('./models/models');
 const cors = require('cors');
 const router = require('./routes/index');
+const fileUpload = require("express-fileupload");
 const errorHandler = require("./middleware/ErrorHandlingMiddleware");
-
+const path = require('path');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.resolve(__dirname,'static')));
+app.use(fileUpload({}));
 app.use('/api',router);
+
 
 // middleware, обрабатывающий ошибки обязательно замыкающий. Next не вызывается!
 app.use(errorHandler); 
