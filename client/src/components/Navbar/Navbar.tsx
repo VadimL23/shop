90f3764c2db9b6381767998fd3376857da8432e6
Vadim <Vadim_lachkov@mail.ru>
2021-08-21 19:35:18 +0700

    add Navbar testing

---
+++ client/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import React,{MouseEvent, useState, useEffect} from "react";
+import s from "./style.module.scss";
+import cn from "classnames";
+
+interface IProps {
+    isAuthenticated:boolean;
+}
+
+const Navbar = (props:IProps) =>{
+ 
+ return ( 
+  <nav>
+    <div className={cn(s.nav)}>
+      <a href="/" className="">Logo shop</a>
+      <ul className={cn(s["nav__links"])}>
+        <li><a href = "#">Home</a></li>
+        <li><a href = "#">About</a></li>
+      </ul>
+    </div>
+  </nav>
+ )   
+}
+
+export {Navbar}
