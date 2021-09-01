19292926220b904c55d8935f5a2fef3c7a232001
Alex <test@mail.ru>
2021-09-01 17:28:26 +0300

    add InfoPage

---
+++ client/src/config/constants/product_data.ts
@@ -0,0 +1,14 @@
+const product_data = { 
+       title: "Миндальный орех", 
+       description:"Миндальный орех очень полезен и насыщен множество микроэлементов, необходимых для развития человека.", 
+       rate: 5, 
+       img: [
+              "http://placehold.it/1200x1600",
+              "http://placehold.it/1200x1600",
+              "http://placehold.it/1200x1600",
+              "http://placehold.it/1200x1600"
+       ]
+}
+
+export default product_data;
+
